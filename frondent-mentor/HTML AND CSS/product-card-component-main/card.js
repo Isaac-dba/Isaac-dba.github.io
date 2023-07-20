a1caaba8ebedc83c629c9e821589d781fe58411b
joel santos <santoaprenderdb@gmail.com>
2023-07-20 21:53:50 +0200

    A-2.0 cart-perfume

---
+++ frondent-mentor/HTML AND CSS/product-card-component-main/card.js
@@ -0,0 +1,7 @@
+const textContainer = document.getElementById("text-container");
+let count = "0px";
+
+console.log("hola", window.screen.width);
+if (window.screen.width >= 654) {
+  console.log(textContainer);
+}
