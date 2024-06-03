910680e2e83199840dec412abb6ee6aace94150d
joel santos <santoaprenderdb@gmail.com>
2024-06-03 19:48:46 +0200

    ajustes generales

--- ejercicios/100DaysCss/JS/JQuery.js
+++ ejercicios/100DaysCss/JS/JQuery.js
@@ -1,4 +1,33 @@
+//elementos html
+const btnBefore = document.getElementById("before");
+const btnAfter = document.getElementById("after");
+const section = document.querySelectorAll("#section-days article");
+const spanNum = document.getElementById("days-number");
+
+//funcionalidad
+let currentId = 0;
+function showArticle(index){
+    section.forEach((article, i) => {
+        article.classList.toggle("active", i === index);
+    });
+}
+
+btnBefore.addEventListener("click", () => {
+    currentId = (currentId > 0) ? currentId - 1 : section.length - 1;
+    showArticle(currentId);
+    spanNum.innerText = currentId + 1;
+});
+btnAfter.addEventListener("click", () => {
+    currentId = (currentId < section.length - 1) ? currentId + 1 : 0;
+    showArticle(currentId);
+    spanNum.innerText = currentId + 1;
+});
+showArticle(currentId);
+//este codigo tambien puede ir dentro del codigo JQuery
+
+
+// parte JQuery
 $(document).ready(function() {
-    // $("body").css("background-color", "white");
-    $(".action").css("display", "flex").css("color", "white");
+    
+
 });
