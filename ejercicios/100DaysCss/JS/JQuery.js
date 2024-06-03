4636f4d42707901d799c456a59d893ace5c60bc9
joel santos <santoaprenderdb@gmail.com>
2024-06-03 23:17:13 +0200

    final day-1 y comienzo day-2

--- ejercicios/100DaysCss/JS/JQuery.js
+++ ejercicios/100DaysCss/JS/JQuery.js
@@ -5,7 +5,7 @@ const section = document.querySelectorAll("#section-days article");
 const spanNum = document.getElementById("days-number");
 
 //funcionalidad
-let currentId = 0;
+let currentId = 1;
 function showArticle(index){
     section.forEach((article, i) => {
         article.classList.toggle("active", i === index);
@@ -22,12 +22,95 @@ btnAfter.addEventListener("click", () => {
     showArticle(currentId);
     spanNum.innerText = currentId + 1;
 });
+
 showArticle(currentId);
 //este codigo tambien puede ir dentro del codigo JQuery
 
 
-// parte JQuery
+// parte JQuery 
+//Day 1
 $(document).ready(function() {
-    
+    $(".days-1").css({
+        "flex-direction": "column",
+        "gap": "10px",
+        "background": "-webkit-linear-gradient(bottom left, #43389f 0%, #4ec6ca 100%)",
+    });
+    $(".number").css({
+        "display": "flex",
+        "margin-left": "40px",
+    });
+    $(".num-1").css({
+        "width": "20px",
+        "background-color": "white",
+        "height": "100px",
+        "box-shadow": "0px 0px 4px #e4e4e4",
+        "position": "absolute",
+        "z-index": "100",
+        "left": "100px",
+        "border-radius": "2px" 
+    });
+    $(".num-1").clone().appendTo(".number").css({
+        "width": "0px",
+        "height": "0px",
+        "position": "relative",
+        "left": "-206px",
+        "top": "1px",
+        "transform": "rotate(45deg)",
+        "border-left": "solid 20px white",
+        "border-right": "solid 0px white",
+        "border-bottom": "solid 20px white",
+        "border-top": "solid 20px transparent",
+        "z-index": "10"
+    });
+    $(".uno").css({
+        "width": "100px",
+        "height": "100px",
+        "background-color": "transparent",
+        "border": "solid 23px white",
+        "border-radius": "50%",
+        "box-shadow": "0px 0px 4px #e4e4e4",
+        "margin-left": "1px",
+        "z-index": "20"
+    });
+    $(".dos").css({
+        "width": "100px",
+        "height": "100px",
+        "background-color": "transparent",
+        "border": "solid 23px white",
+        "border-radius": "50%",
+        "box-shadow": "0px 0px 4px #e4e4e4",
+        "margin-left": "-15px"
+        
+    });
+    $(".text-days").css({
+        "color": "white",
+        "font-weight": "bold",
+        "font-size": "4.4rem",
+        "margin-top": "-20px"
+    });
+    $(".text-info").css({
+        "color": "white",
+        "letter-spacing": "4px",
+        "font-weight": "bold",
+        "margin-top": "-20px"
+    });
 
+//Day 2
+    $(".days-2").css({
+        "background-color": "#39b98f"
+    });
+    $(".content-menu").css({
+        "display": "flex",
+        "flex-direction": "column",
+        "gap": "10px"
+    });
+    $(".item").css({
+        "width": "80px",
+        "height": "10px",
+        "background-color": "white",
+        "border": "none",
+        "border-radius": "3px",
+        "box-shadow": "0px 3px 4px #686767"
+    });
 });
+
