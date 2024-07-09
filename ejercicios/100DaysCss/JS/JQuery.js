36f4ff23d11ce0f604f150e4246032db9a269c1c
joel santos <santoaprenderdb@gmail.com>
2024-07-09 21:19:41 +0200

    days 23 aprendiendo svg

--- ejercicios/100DaysCss/JS/JQuery.js
+++ ejercicios/100DaysCss/JS/JQuery.js
@@ -5,7 +5,7 @@ const section = document.querySelectorAll("#section-days article");
 const spanNum = document.getElementById("days-number");
 
 //funcionalidad
-let currentId = 22;
+let currentId = 0;
 function showArticle(index){
     section.forEach((article, i) => {
         article.classList.toggle("active", i === index);
