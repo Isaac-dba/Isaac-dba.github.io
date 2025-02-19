47e379c6ec64d8c101a25c59c8697b0105591400
joel santos <santoaprenderdb@gmail.com>
2025-02-19 19:58:47 +0100

    days 63 scale primero

--- ejercicios/100DaysCss/JS/JQuery.js
+++ ejercicios/100DaysCss/JS/JQuery.js
@@ -5,7 +5,7 @@ const section = document.querySelectorAll("#section-days article");
 const spanNum = document.getElementById("days-number");
 
 //funcionalidad
-let currentId = 61;
+let currentId = 62;
 function showArticle(index){
     section.forEach((article, i) => {
         article.classList.toggle("active", i === index);
