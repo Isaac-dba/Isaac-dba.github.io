15b9084c3d12c3a5e816140de3e1be8cbf18ddb0
joel santos <santoaprenderdb@gmail.com>
2025-03-06 20:21:34 +0100

    day 76 fin

--- ejercicios/100DaysCss/JS/JQuery.js
+++ ejercicios/100DaysCss/JS/JQuery.js
@@ -5,7 +5,7 @@ const section = document.querySelectorAll("#section-days article");
 const spanNum = document.getElementById("days-number");
 
 //funcionalidad
-let currentId = 74;
+let currentId = 75;
 function showArticle(index){
     section.forEach((article, i) => {
         article.classList.toggle("active", i === index);
